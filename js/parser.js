3b457d6e96b7a7d976437a82855a845fb92b6c66
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-04 22:58:58 -0500

    Update parser.js

--- js/parser.js
+++ js/parser.js
@@ -1,30 +1,30 @@
+function menu_click(x) {
+
+	while (bottom_scrollbar.firstChild) {
+		bottom_scrollbar.removeChild(bottom_scrollbar.firstChild);
+	}
+
+	for (y in trophy_list[x]) {
+		if (trophy_list[x][y].constructor === Object){
+
+			var mini_div = document.createElement("div");
+			mini_div.setAttribute("id", y);
+			mini_div.setAttribute("class", "mini_tab");
+			mini_div.setAttribute("onclick", "load_page(this.id)");
+			mini_div.innerHTML = trophy_list[x][y].name;
+
+			document.getElementById("bottom_scrollbar").appendChild(mini_div);
+
+		}
+	}
+
+	load_page(x);
+
+};
+
 window.onload = function(){
 
 	var bottom_scrollbar = document.getElementById("bottom_scrollbar");
-
-	function menu_click(x) {
-
-		while (bottom_scrollbar.firstChild) {
-			bottom_scrollbar.removeChild(bottom_scrollbar.firstChild);
-		}
-
-		for (y in trophy_list[x]) {
-			if (trophy_list[x][y].constructor === Object){
-
-				var mini_div = document.createElement("div");
-				mini_div.setAttribute("id", y);
-				mini_div.setAttribute("class", "mini_tab");
-				mini_div.setAttribute("onclick", "load_page(this.id)");
-				mini_div.innerHTML = trophy_list[x][y].name;
-
-				document.getElementById("bottom_scrollbar").appendChild(mini_div);
-
-			}
-		}
-
-		load_page(x);
-
-	};
 
 	for (x in trophy_list) {
 
