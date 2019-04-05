742ea1196ae1571f36040e3a22424691e66c658b
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-04 23:59:04 -0500

    Update parser.js

--- js/parser.js
+++ js/parser.js
@@ -1,4 +1,4 @@
-function menu_click(x) {
+function menu_click(x){
 
 	while (bottom_scrollbar.firstChild) {
 		bottom_scrollbar.removeChild(bottom_scrollbar.firstChild);
@@ -13,18 +13,61 @@ function menu_click(x) {
 			mini_div.setAttribute("onclick", "load_page(this.id)");
 			mini_div.innerHTML = trophy_list[x][y].name;
 
-			document.getElementById("bottom_scrollbar").appendChild(mini_div);
+			bottom_scrollbar.appendChild(mini_div);
 
 		}
 	}
 
 	load_page(x);
 
-};
+}
+
+var save_data = {
+
+	data : {},
+
+	load : function(){
+
+		JSON.parse(localStorage.getItem("progress")) === null ? (this.data = empty_progress) : (this.data = JSON.parse(localStorage.getItem("progress")));
+
+	},
+	
+	save : function(){
+
+		localStorage.setItem("progress", JSON.stringify(this.data));
+		console.log(this.data);
+
+	}
+
+}
+
+function load_page(x){
+
+	save_data.load();
+
+	while (page.firstChild) {
+		page.removeChild(page.firstChild);
+	}
+
+	switch (x){
+		case 'world_boss_kills':
+		case 'elite_boss_kills':
+			for (y in save_data.data[x]){
+				var cell_top = document.createElement("div");
+				cell_top.innerHTML = trophy_list[x][y].name;
+				page.appendChild(cell_top);
+				var cell_bot = document.createElement("div");
+				cell_bot.innerHTML = save_data.data[x][y];
+				page.appendChild(cell_bot);
+			}
+			break;
+	}
+}
 
 window.onload = function(){
 
 	var bottom_scrollbar = document.getElementById("bottom_scrollbar");
+	var page = document.getElementById("page");
 
 	for (x in trophy_list) {
 
@@ -38,4 +81,4 @@ window.onload = function(){
 
 	};
 
-};
+}
