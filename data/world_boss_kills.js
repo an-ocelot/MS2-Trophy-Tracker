70307236b3e357141c58031658c56fa00fc5223f
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-06 23:12:29 -0500

    Create world_boss_kills.js

---
+++ data/world_boss_kills.js
@@ -0,0 +1,30 @@
+data.world_boss_kills = {
+		
+	name : "World Boss Kills",
+		
+	acreon : {
+		name : "Acreon",
+		trophyname : "The Ascending Horned Messenger",
+		tiers : {
+			1 : 1,
+			2 : 10,
+			3 : 50,
+			4 : 100,
+			5 : 300
+		}
+	},
+		
+	chky : {
+		name : "CHKY-002",
+		trophyname : "Secret Weapon of the Resistance",
+		tiers : {
+			1 : 1,
+			2 : 10,
+			3 : 50,
+			4 : 100,
+			5 : 300
+		}
+
+	}
+
+}
