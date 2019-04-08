308126df51da00647e6f68003c92efeade338fef
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-08 12:49:42 -0500

    Create boss_trophies.js

---
+++ data/boss_trophies.js
@@ -0,0 +1,56 @@
+data.boss_trophies = {
+
+  name : "Boss Trophies",
+  
+  type : "normal",
+  
+  veri : "some key",
+  
+  world_bosses : {
+  
+    alpha_turtle : {
+    
+      size : 1,
+      name : "Alpha Turtle"
+    
+    },
+    
+    lo_and_moomoo : {
+    
+      size : 2,
+      name : "Lo and Moomoo"
+    
+    },
+    
+    toh_and_googoo : {
+    
+      size : 2,
+      name : "Toh and Googoo"
+    
+    },
+    
+    giant_turtle : {
+    
+      size : 3,
+      name : "Giant Turtle"
+    
+    },
+    
+    griffin : {
+    
+      size : 1,
+      name : "Griffin"
+    
+    },
+    
+    griffina : {
+    
+      size : 1,
+      name : "Griffina"
+    
+    }
+  
+  }
+
+
+}
