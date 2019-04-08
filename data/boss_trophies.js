7e64dad9bce4925789d0c980b421c69f338a368b
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-08 12:57:57 -0500

    Update boss_trophies.js

--- data/boss_trophies.js
+++ data/boss_trophies.js
@@ -11,46 +11,125 @@ data.boss_trophies = {
     alpha_turtle : {
     
       size : 1,
-      name : "Alpha Turtle"
+      name : "Alpha Turtle",
+      
+      trophy1 : {
+        
+        name : "Trampled by a Raging Turtle",
+        desc : "Get Tombstoned by spinning horn strike",
+        amnt : "1/10/100"
+        
+      }
     
     },
     
     lo_and_moomoo : {
     
       size : 2,
-      name : "Lo and Moomoo"
+      name : "Lo and Moomoo",
+      
+      trophy1 : {
+        
+        name : "Shimmering Countenance",
+        desc : "Suffer Maskless Attack",
+        amnt : "1/100"
+        
+      },
+    
+      trophy2 : {
+        
+        name : "Shrouded by the Forest",
+        desc : "Defeat Lo",
+        amnt : "1/50/200"
+        
+      }
     
     },
     
     toh_and_googoo : {
     
       size : 2,
-      name : "Toh and Googoo"
+      name : "Toh and Googoo",
+      
+      trophy1 : {
+        
+        name : "A Terrible Visage",
+        desc : "Suffer Maskless Attack",
+        amnt : "1/100"
+        
+      },
+    
+      trophy2 : {
+        
+        name : "Cloaked in Dust",
+        desc : "Defeat Toh",
+        amnt : "1/50/200"
+        
+      }
     
     },
     
     giant_turtle : {
     
       size : 3,
-      name : "Giant Turtle"
+      name : "Giant Turtle",
+      
+      trophy1 : {
+        
+        name : "Blargh!",
+        desc : "Vomit",
+        amnt : "1"
+        
+      },
+    
+      trophy2 : {
+        
+        name : "I Can't Breathe!",
+        desc : "Get trapped in water",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Turtle Trouble",
+        desc : "Complete previous trophies",
+        amnt : "2"
+        
+      }
     
     },
     
     griffin : {
     
       size : 1,
-      name : "Griffin"
+      name : "Griffin",
+      
+      trophy1 : {
+        
+        name : "Fly Griffin Airlines",
+        desc : "Fly by grabbing on",
+        amnt : "1"
+        
+      }
     
     },
     
     griffina : {
     
       size : 1,
-      name : "Griffina"
+      name : "Griffina",
+      
+      trophy1 : {
+        
+        name : "Fly Griffina Airlines",
+        desc : "Fly by grabbing on",
+        amnt : "1"
+        
+      }
     
     }
   
   }
-
 
 }
