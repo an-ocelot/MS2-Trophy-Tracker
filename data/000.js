1510c1ec6871637b1c0ed029b67ce6aa5977532a
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-10 18:18:47 -0500

    Create 000.js

---
+++ data/000.js
@@ -0,0 +1,33 @@
+//Data example file
+data.example = {
+  
+  name : "name", //Sheet name
+  
+  size_c : 15, //Sheet size in columns
+    
+  size_r : 20, //Sheet size in rows
+  
+  category : { //A category stretches the entire row
+    
+    name : "name", //Category name
+    
+    type : "type", //Category name description
+  
+    group1 : {
+    
+      size : 1, //Amount of trophies
+      name : "name", //Group name
+      
+      trophy1 : {
+        
+        name : "name", //Trophy name
+        desc : "desc", //Trophy description
+        amnt : "amnt" //Trophy amount
+        
+      }
+
+    }
+  
+  }
+
+}
