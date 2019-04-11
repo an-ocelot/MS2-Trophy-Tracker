ae9b352b4ab099b4cd09716ab54404c96d2d2c93
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-11 14:30:52 -0500

    Update classes.js

--- data/classes.js
+++ data/classes.js
@@ -41,6 +41,285 @@ data.classes = {
         
       }
 
+    },
+    
+    assassin : {
+    
+      size : 3,
+      name : "Assassin",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    berserker : {
+    
+      size : 3,
+      name : "Berserker",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    heavy_gunner : {
+    
+      size : 3,
+      name : "Heavy Gunner",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    knight : {
+    
+      size : 3,
+      name : "Knight",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    priest : {
+    
+      size : 3,
+      name : "Priest",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    runeblade : {
+    
+      size : 3,
+      name : "Runeblade",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    soul_binder : {
+    
+      size : 3,
+      name : "Soul Binder",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    thief : {
+    
+      size : 3,
+      name : "Thief",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    wizard : {
+    
+      size : 3,
+      name : "Wizard",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
     }
   
   }
