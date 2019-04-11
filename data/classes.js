6797a55983e3df5fee4acd0140707818fdae9655
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-11 13:03:57 -0500

    Update classes.js

--- data/classes.js
+++ data/classes.js
@@ -1,1 +1,48 @@
-data.classes = {}
+data.classes = {
+
+  name : "Class Trophies",
+  
+  size_c : 15,
+    
+  size_r : 20,
+  
+  class_level : {
+    
+    name : "Class Trophies",
+    
+    type : "Class",
+  
+    archer : {
+    
+      size : 3,
+      name : "Archer",
+      
+      trophy1 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 30",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 40",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "name",//****************************************
+        desc : "Reach level 50",
+        amnt : "1"
+        
+      }
+
+    }
+  
+  }
+
+}
