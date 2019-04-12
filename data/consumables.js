7036a9d523317b406c2a67da79e47df81e2beca7
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-11 22:23:51 -0500

    Update consumables.js

--- data/consumables.js
+++ data/consumables.js
@@ -1,1 +1,110 @@
-data.consumables = {}
+data.consumables = {
+
+  name : "Consumables",
+  
+  size_c : 50,
+    
+  size_r : 50,
+  
+  specific_locations : {
+    
+    name : "Meso Consumables",
+    
+    type : "Location",
+  
+    lith_harbor : {
+    
+      size : 3,
+      name : "Lith Harbor",
+      
+      trophy1 : {
+        
+        name : "Carbonated Cherryade",
+        desc : "Drink Cherryade",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Refreshing Strawberry Punch",
+        desc : "Drink Strawberry Punch",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Bubbly and Refreshing",
+        desc : "Drink Bubbly Grape Drink",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    },
+    
+    craft_shop : {
+    
+      size : 3,
+      name : "Craft Shop",
+      
+      trophy1 : {
+        
+        name : "Tart Redberry",
+        desc : "Eat Redberries",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Tart Blueberry",
+        desc : "Eat Blueberries",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Tart Whiteberry",
+        desc : "Eat Whiteberries",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    },
+    
+    minigame_shop : {
+    
+      size : 3,
+      name : "Sole Survivor/Trap Master",
+      
+      trophy1 : {
+        
+        name : "Chewy Squid",
+        desc : "Eat Buttered Squid",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Ramen with Attitude",
+        desc : "Eat Springy Ramen",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Briny Grilled Jumbo Shrimp",
+        desc : "Eat Grilled Shrimp",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    }
+  
+  }
+
+}
