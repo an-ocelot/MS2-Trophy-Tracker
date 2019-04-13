cf459cda74c841ed40ea1c766aff52224628dc91
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-12 23:20:39 -0500

    Update and rename experience.js to exp_time.js

---
+++ data/exp_time.js
@@ -0,0 +1,86 @@
+data.exp_time = {
+  
+  name : "Experience/Time Trophies",
+  
+  size_c : 15,
+    
+  size_r : 20,
+  
+  experience : {
+    
+    name : "Experience",
+    
+    type : "Type",
+  
+    exp : {
+    
+      size : 2,
+      name : "Exp Trophies",
+      
+      trophy1 : {
+        
+        name : "The Journey Continues",
+        desc : "Get Experience",
+        amnt : "1000000000/3000000000/5000000000/10000000000/30000000000/50000000000/100000000000/1000000000000/2015070719000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Every End is a New Beginning",
+        desc : "Get Experience after lv.50",
+        amnt : "500000/2000000000/3000000000/6000000000/10000000000/20000000000/30000000000/50000000000/100000000000"
+        
+      }
+
+    },
+    
+    prestige : {
+    
+      size : 2,
+      name : "Prestige Trophies",
+      
+      trophy1 : {
+        
+        name : "The Adventure Continues",
+        desc : "Reach prestige level",
+        amnt : "10/20/30/40/50/60/70/80/90"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "A True Adventurer",
+        desc : "Reach prestige level",
+        amnt : "100"
+        
+      }
+
+    }
+  
+  },
+  
+  time : {
+    
+    name : "Time",
+    
+    type : "Type",
+    
+    time : {
+    
+      size : 1,
+      name : "Time",
+      
+      trophy1 : {
+        
+        name : "Time Among Friends",
+        desc : "Reach cumulative play time",
+        amnt : "1/10/50/100/500/2400/4800/8760/24000"
+        
+      }
+
+    }
+  
+  }
+  
+}
