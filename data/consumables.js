2157154877b534b8229dd387b7852875c2cf554c
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-12 21:54:55 -0500

    Update consumables.js

--- data/consumables.js
+++ data/consumables.js
@@ -105,6 +105,455 @@ data.consumables = {
 
     }
   
+  },
+  
+  black_market : {
+    
+    name : "Tradable Consumables",
+    
+    type : "Consumables",
+  
+    fishing : {
+    
+      size : 21,
+      name : "Fishing Consumables (available in Black Market or from Heavy/Lightweight Clam Boxes)",
+      
+      trophy1 : {
+        
+        name : "Company, Attention!",
+        desc : "Use Guard Whistle",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Is Anybirdie Out There?",
+        desc : "Blow Eka's Flute Whistle",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Just One Sip",
+        desc : "Drink Mysterious Cucumber Cocktail",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Shake Shake Boom!",
+        desc : "Use Volatile Mixture",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Burning Passion",
+        desc : "Use Terry's Sailing Chart",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Merry-Go-Roast",
+        desc : "Use Barbecue Spit",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy7 : {
+        
+        name : "Leave Them in the Dust",
+        desc : "Use Big Sweep Broom",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy8 : {
+        
+        name : "An Unlikely Evolution",
+        desc : "Use Spring Vine",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy9 : {
+        
+        name : "Cyber-Sleuth",
+        desc : "Use Solvay Hacking Kit",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy10 : {
+        
+        name : "Hey Look at Me!",
+        desc : "Use Attention Grabber",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy11 : {
+        
+        name : "Up in Smoke",
+        desc : "Use Kitty Smoke Bomb",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy12 : {
+        
+        name : "A Short Break",
+        desc : "Use Erryday Rest-o-matic",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy13 : {
+        
+        name : "Lovestruck",
+        desc : "Look into Mika Fanart",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy14 : {
+        
+        name : "WHARRGARBL",
+        desc : "Use Flowing Water Scroll",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy15 : {
+        
+        name : "Living Large",
+        desc : "Use Growth Enhancer",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy16 : {
+        
+        name : "Standing in the Spotlight",
+        desc : "Use Spotlight",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy17 : {
+        
+        name : "It's So Shiny!",
+        desc : "Use Fairy Dust",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy18 : {
+        
+        name : "An Icy Silence",
+        desc : "Read Big Book of Dad Jokes",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy19 : {
+        
+        name : "If at First You Don't Succeed",
+        desc : "Read Momo's Failure Stories",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy20 : {
+        
+        name : "A Natural Fragrance",
+        desc : "Drink Tea Leaf Jar",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy21 : {
+        
+        name : "Warm Memories",
+        desc : "Use Campfire Kit",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    }
+  
+  },
+  
+  assistant : {
+    
+    name : "Assistant Consumables",
+    
+    type : "Consumable",
+  
+    drinks : {
+    
+      size : 6,
+      name : "Consume drinks",
+      
+      trophy1 : {
+        
+        name : "Sweet Rainbow Shake",
+        desc : "Drink Rainbow Shake",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Sour Orange Refreshment",
+        desc : "Drink Orange Cooler",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Berry Aggressive Beverage",
+        desc : "Drink Strawberry Knock-out",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Carbonated Lemonade",
+        desc : "Drink Lemonade",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Carbonated Grapefruitade",
+        desc : "Drink Grapefruitade",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Liquid Diet",
+        desc : "Drink Protien Juice",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    },
+    
+    foods : {
+      
+      size : 8,
+      name : "Consume foods",
+      
+      trophy1 : {
+        
+        name : "Plump Rotisserie Chicken",
+        desc : "Eat Plump Chicken",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Get in My Belly",
+        desc : "Eat Roasted Pork Belly",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Beans n' Noodles",
+        desc : "Eat Black Bean Noodles",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Fiery Rice Cake",
+        desc : "Eat Spicy Rice Cake",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Sweet Seasoned Chicken",
+        desc : "Eat Marinated Chicken",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Yogurt for a Healthy Gut",
+        desc : "Eat Yogurt",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy7 : {
+        
+        name : "Snow Cream Shortcake",
+        desc : "Eat Snow Cream Shortcake",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy8 : {
+        
+        name : "Lemon Fried Shrimp",
+        desc : "Eat Lemon Fried Shrimp",
+        amnt : "1/50/100/500/1000"
+        
+      }
+      
+    }
+    
+  },
+  
+  level_up : {
+    
+    name : "Adventurer's Food Box",
+    
+    type : "Consumables",
+  
+    items : {
+    
+      size : 4,
+      name : "Consume drinks",
+      
+      trophy1 : {
+        
+        name : "Cleared for Takeoff",
+        desc : "Drink High Flyer",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Refreshingly Sweet and Cool",
+        desc : "Drink Sweet Juice Blend",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Grilled to Perfection",
+        desc : "Eat Grilled Chicken",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Cheese Cupcake",
+        desc : "Eat Cheese Cupcake",
+        amnt : "1/50/100/500/1000"
+        
+      }
+
+    }
+    
+  },
+  
+  consuming : {
+    
+    name : "Consume things",
+    
+    type : "Type",
+  
+    healing : {
+    
+      size : 1,
+      name : "Consume hp potions",
+      
+      trophy1 : {
+        
+        name : "Good Herb",
+        desc : "Restore health with potions",
+        amnt : "1/100/500/1000/3000/5000/10000/100000/1000000"
+        
+      }
+
+    },
+    
+    elixer : {
+    
+      size : 1,
+      name : "Consume elixers",
+      
+      trophy1 : {
+        
+        name : "The Wonder-Tonic",
+        desc : "Restore health with elixers",
+        amnt : "1/100/500/1000/5000/10000/50000/100000/1000000"
+        
+      }
+
+    }
+  
+  },
+  
+  combination : {
+    
+    name : "Combination",
+    
+    type : "Consumables",
+  
+    items : {
+    
+      size : 4,
+      name : "Collect trophies",
+      
+      trophy1 : {
+        
+        name : "Late-Night Snack",
+        desc : "Collect late-night snack trophies",
+        amnt : "3/6/9/12/15"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Bubbly and Refreshing",
+        desc : "Collect -ade trophies",
+        amnt : "3/6/9/12/15"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Life's Greatest Pleasure",
+        desc : "Collect joyous food trophies",
+        amnt : "3/6/9/12/15"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Fire-Grilled Food",
+        desc : "Collect grilled food trophies",
+        amnt : "3/6/9/12/15"
+        
+      }
+
+    }
+    
   }
 
 }
