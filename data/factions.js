44f452529167ce6bd0cfdeadd77e8a8f5509708d
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-13 21:06:02 -0500

    Update factions.js

--- data/factions.js
+++ data/factions.js
@@ -1,1 +1,260 @@
-data.factions = {}
+data.factions = {
+  
+  name : "Factions",
+  
+  size_c : 15,
+    
+  size_r : 20,
+  
+  faction_spec : {
+    
+    name : "Faction Specific Trophies",
+    
+    type : "Type",
+    
+    green_hoods : {
+    
+      size : 6,
+      name : "Green Hoods",
+      
+      trophy1 : {
+        
+        name : "Militia Volunteer",
+        desc : "Be trusted",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Militia Captain",
+        desc : "Be respected",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Hero of Green Hoods",
+        desc : "Be beloved",
+        amnt : "1"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Arrows of the Gods",
+        desc : "Be revered",
+        amnt : "1"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Archer and Aide",
+        desc : "Talk to Nairin",
+        amnt : "1"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Philanthropist",
+        desc : "Donate items",
+        amnt : "1/10/25/50/100"
+        
+      }
+      
+    },
+    
+    lumiknight : {
+    
+      size : 5,
+      name : "Lumiknights",
+      
+      trophy1 : {
+        
+        name : "Lumiknight Squire",
+        desc : "Be trusted",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Lumiknight",
+        desc : "Be respected",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Ascended Lumiknight",
+        desc : "Be beloved",
+        amnt : "1"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Templar",
+        desc : "Be revered",
+        amnt : "1"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Master of Arcanobabble",
+        desc : "Talk to Mason",
+        amnt : "1"
+        
+      }
+
+    },
+  
+    dark_wind : {
+    
+      size : 3,
+      name : "Dark Wind",
+      
+      trophy1 : {
+        
+        name : "Dark Wind Operative",
+        desc : "Be trusted",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Dark Wind Captain",
+        desc : "Be respected",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Secret Agent",
+        desc : "Be beloved",
+        amnt : "1"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Spy Master",
+        desc : "Be revered",
+        amnt : "1"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Let's Get Closer",
+        desc : "Talk to Schatten",
+        amnt : "1"
+        
+      }
+ 
+  },
+  
+  royal_guard : {
+    
+    size : 5,
+    name : "Royal Guard",
+      
+    trophy1 : {
+        
+      name : "Royal Guard Cadet",
+      desc : "Be trusted",
+      amnt : "1"
+        
+    },
+      
+    trophy2 : {
+        
+      name : "Rising Royal Guard",
+      desc : "Be respected",
+      amnt : "1"
+        
+    },
+      
+    trophy3 : {
+        
+      name : "Elite Royal Guard",
+      desc : "Be beloved",
+      amnt : "1"
+        
+    },
+      
+    trophy4 : {
+        
+      name : "Shield of Tria",
+      desc : "Be revered",
+      amnt : "1"
+        
+    },
+      
+    trophy5 : {
+        
+      name : "Eternal Drill Sergeant",
+      desc : "Talk to Condor",
+      amnt : "1"
+        
+    }
+  
+  },
+  
+  maple_alliance : {
+    
+      size : 5,
+      name : "Reputation",
+      
+    trophy1 : {
+        
+      name : "Alliance Soldier",
+      desc : "Be trusted",
+      amnt : "1"
+        
+    },
+      
+    trophy2 : {
+        
+      name : "Alliance Veteran",
+      desc : "Be respected",
+      amnt : "1"
+        
+    },
+      
+    trophy3 : {
+        
+      name : "Alliance Officer",
+      desc : "Be beloved",
+      amnt : "1"
+        
+    },
+      
+    trophy4 : {
+        
+      name : "Alliance Commander",
+      desc : "Be revered",
+      amnt : "1"
+        
+    },
+    
+    trophy1 : {
+        
+      name : "The Missing Navy",
+      desc : "Talk to Veliche",
+      amnt : "1"
+        
+    }
+  
+  },
+    
+    
+  
+}
