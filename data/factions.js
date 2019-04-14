f11b541ebfc714bcf8971b8ea7461080871c38b0
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-13 22:11:20 -0500

    Update factions.js

--- data/factions.js
+++ data/factions.js
@@ -159,102 +159,338 @@ data.factions = {
         
       }
  
-  },
+    },
   
-  royal_guard : {
-    
-    size : 5,
-    name : "Royal Guard",
-      
-    trophy1 : {
-        
-      name : "Royal Guard Cadet",
-      desc : "Be trusted",
-      amnt : "1"
-        
+    royal_guard : {
+
+      size : 5,
+      name : "Royal Guard",
+
+      trophy1 : {
+
+        name : "Royal Guard Cadet",
+        desc : "Be trusted",
+        amnt : "1"
+
+      },
+
+      trophy2 : {
+
+        name : "Rising Royal Guard",
+        desc : "Be respected",
+        amnt : "1"
+
+      },
+
+      trophy3 : {
+
+        name : "Elite Royal Guard",
+        desc : "Be beloved",
+        amnt : "1"
+
+      },
+
+      trophy4 : {
+
+        name : "Shield of Tria",
+        desc : "Be revered",
+        amnt : "1"
+
+      },
+
+      trophy5 : {
+
+        name : "Eternal Drill Sergeant",
+        desc : "Talk to Condor",
+        amnt : "1"
+
+      }
+
     },
-      
-    trophy2 : {
-        
-      name : "Rising Royal Guard",
-      desc : "Be respected",
-      amnt : "1"
-        
-    },
-      
-    trophy3 : {
-        
-      name : "Elite Royal Guard",
-      desc : "Be beloved",
-      amnt : "1"
-        
-    },
-      
-    trophy4 : {
-        
-      name : "Shield of Tria",
-      desc : "Be revered",
-      amnt : "1"
-        
-    },
-      
-    trophy5 : {
-        
-      name : "Eternal Drill Sergeant",
-      desc : "Talk to Condor",
-      amnt : "1"
-        
-    }
-  
-  },
-  
-  maple_alliance : {
-    
+
+    maple_alliance : {
+
       size : 5,
       name : "Reputation",
-      
-    trophy1 : {
-        
-      name : "Alliance Soldier",
-      desc : "Be trusted",
-      amnt : "1"
-        
-    },
-      
-    trophy2 : {
-        
-      name : "Alliance Veteran",
-      desc : "Be respected",
-      amnt : "1"
-        
-    },
-      
-    trophy3 : {
-        
-      name : "Alliance Officer",
-      desc : "Be beloved",
-      amnt : "1"
-        
-    },
-      
-    trophy4 : {
-        
-      name : "Alliance Commander",
-      desc : "Be revered",
-      amnt : "1"
-        
+
+      trophy1 : {
+
+        name : "Alliance Soldier",
+        desc : "Be trusted",
+        amnt : "1"
+
+      },
+
+      trophy2 : {
+
+        name : "Alliance Veteran",
+        desc : "Be respected",
+        amnt : "1"
+
+      },
+
+      trophy3 : {
+
+        name : "Alliance Officer",
+        desc : "Be beloved",
+        amnt : "1"
+
+      },
+
+      trophy4 : {
+
+        name : "Alliance Commander",
+        desc : "Be revered",
+        amnt : "1"
+
+      },
+
+      trophy5 : {
+
+        name : "The Missing Navy",
+        desc : "Talk to Veliche",
+        amnt : "1"
+
+      }
+
     },
     
-    trophy1 : {
-        
-      name : "The Missing Navy",
-      desc : "Talk to Veliche",
-      amnt : "1"
-        
+    rumble : {
+
+      size : 6,
+      name : "Fortress Rumble",
+
+      trophy1 : {
+
+        name : "Dedicated Rumbler",
+        desc : "Complete all weeklies",
+        amnt : "5"
+
+      },
+      
+      trophy2 : {
+
+        name : "Master of the Rumble",
+        desc : "Earn an A Rank",
+        amnt : "1"
+
+      },
+      
+      trophy3 : {
+
+        name : "Fortress Rumble Master",
+        desc : "Earn S+ Ranks in all rumbles",
+        amnt : "1"
+
+      },
+
+      trophy4 : {
+
+        name : "Simulator Technician",
+        desc : "Complete Green Hood Weekly",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy5 : {
+
+        name : "Simulation Complete",
+        desc : "Earn S+ Rank in Green Hood",
+        amnt : "1"
+
+      },
+
+      trophy6 : {
+
+        name : "The Rising Templar",
+        desc : "Complete Lumiknight Weekly",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy7 : {
+
+        name : "The Templar Falls",
+        desc : "Earn S+ Rank in Lumiknight",
+        amnt : "1"
+
+      },
+
+      trophy8 : {
+
+        name : "Master of Shadow",
+        desc : "Complete Dark Wind Weekly",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy9 : {
+
+        name : "Walk in the Shadows",
+        desc : "Earn S+ Rank in Dark Wind",
+        amnt : "1"
+
+      },
+
+      trophy10 : {
+
+        name : "Champion of the Guard",
+        desc : "Complete Royal Guard Weekly",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy11 : {
+
+        name : "Condor Down",
+        desc : "Earn S+ Rank in Royal Guard",
+        amnt : "1"
+
+      },
+      
+      trophy12 : {
+
+        name : "The Captain's Secret Weapon",
+        desc : "Complete Maple Alliance Weekly",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy12 : {
+
+        name : "Captain's Keeper",
+        desc : "Earn S+ Rank in Maple Alliance",
+        amnt : "1"
+
+      }
+
     }
-  
+    
   },
+  
+  any_faction : {
     
+    name : "Faction Trophies",
     
+    type : "Type",
+    
+    reputation : {
+      
+      size : 4,
+      name : "Reputation",
+      
+      trophy1 : {
+
+        name : "Sky Fortress Gopher",
+        desc : "Be trusted by any faction",
+        amnt : "1"
+
+      },
+
+      trophy2 : {
+
+        name : "Sky Fortress Crewman",
+        desc : "Be respected by any faction",
+        amnt : "1"
+
+      },
+
+      trophy3 : {
+
+        name : "Sky Fortress Officer",
+        desc : "Be beloved by any faction",
+        amnt : "1"
+
+      },
+
+      trophy4 : {
+
+        name : "Sky Fortress' Hero",
+        desc : "Be revered by any faction",
+        amnt : "1"
+
+      }
+      
+    },
+    
+    field_missions : {
+      
+      size : 3,
+      name : "Field Missions",
+      
+      trophy1 : {
+
+        name : "Worker Bee",
+        desc : "Complete several field missions",
+        amnt : "1"
+
+      },
+      
+      trophy2 : {
+
+        name : "Field Agent",
+        desc : "Complete field missions",
+        amnt : "1/10/25/50/100"
+
+      },
+
+      trophy3 : {
+
+        name : "Boots on the Ground",
+        desc : "Use Sortie System",
+        amnt : "50"
+
+      }
+      
+    },
+    
+    quests : {
+      
+      size : 5,
+      name : "Quests",
+      
+      trophy1 : {
+
+        name : "D-Class Specialist",
+        desc : "Complete D-Class Quests",
+        amnt : "1/10/25/50/100"
+
+      },
+
+      trophy2 : {
+
+        name : "C-Class Responder",
+        desc : "Complete C-Class Quests",
+        amnt : "1/10/25/50/100"
+
+      },
+
+      trophy3 : {
+
+        name : "B-Class Operator",
+        desc : "Complete B-Class Quests",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy4 : {
+
+        name : "A-Class Enforcer",
+        desc : "Complete A-Class Quests",
+        amnt : "1/10/25/50/100"
+
+      },
+      
+      trophy5 : {
+
+        name : "S-Class Strategist",
+        desc : "Complete B-Class Quests",
+        amnt : "1/10/25/50/100"
+
+      }
+      
+    }
+    
+  }
   
 }
