993f337a80330c08cc66dff384383a692ad3a166
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-13 22:39:04 -0500

    Update fishing.js

--- data/fishing.js
+++ data/fishing.js
@@ -1,1 +1,32 @@
-data.fishing = {}
+data.fishing = {
+  
+  name : "Fishing",
+  
+  size_c : 15,
+    
+  size_r : 20,
+  
+  category : {
+    
+    name : "name",
+    
+    type : "type",
+  
+    group1 : {
+    
+      size : 1, //Amount of trophies
+      name : "name", //Group name
+      
+      trophy1 : {
+        
+        name : "name", //Trophy name
+        desc : "desc", //Trophy description
+        amnt : "amnt" //Trophy amount
+        
+      }
+
+    }
+  
+  }
+
+}
