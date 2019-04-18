ab1c81ba0a3a12348210c315312499bf00e39e22
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-18 15:40:12 -0500

    Update normal_dungeons.js

--- data/normal_dungeons.js
+++ data/normal_dungeons.js
@@ -1,1 +1,891 @@
-data.normal_dungeons = {}
+data.normal_dungeons = {
+  
+  name : "Normal Dungeons",
+  
+  size_c : 50,
+    
+  size_r : 50,
+  
+  hundred_runs : {
+    
+    name : "100 Run Dungeons",
+    
+    type : "Dungeon",
+  
+    cherry_blossom_forest : {
+    
+      size : 3,
+      name : "Cherry Blossom Forest",
+      
+      trophy1 : {
+        
+        name : "The Boro Code",
+        desc : "Rescue Boros",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Princess Yomi's Knight",
+        desc : "Defeat Lazy Stumpy",
+        amnt : "1/10/50"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Yomi, Oh My",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    toxic_garden : {
+    
+      size : 2,
+      name : "Toxic Garden",
+      
+      trophy1 : {
+        
+        name : "Caring for Your Garden",
+        desc : "Defeat Mobs",
+        amnt : "10/100/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Weedkiller",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    golden_tower_elevator : {
+    
+      size : 1,
+      name : "Golden Tower Elevator",
+      
+      trophy1 : {
+        
+        name : "Rock Bottom",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    poison_cave : {
+    
+      size : 4,
+      name : "Poison Cave",
+      
+      trophy1 : {
+        
+        name : "Mysterious Medicine",
+        desc : "Cleanse poison",
+        amnt : "10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Nelph's Story",
+        desc : "Obtain storybooks",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Valuables and Vitriol",
+        desc : "Obtain gold items",
+        amnt : "1"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "The Poison Cave",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    twinkling_path : {
+    
+      size : 2,
+      name : "Twinkling path",
+      
+      trophy1 : {
+        
+        name : "Guardian Angel",
+        desc : "Keep the children safe",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Hide-and-Seek Hero",
+        desc : "Defeat Casse Mu",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    palace_undercroft : {
+    
+      size : 6,
+      name : "Palace Undercroft",
+      
+      trophy1 : {
+        
+        name : "Palace Undercroft Custodian",
+        desc : "Open a door or clear debris",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Belle of the Ball",
+        desc : "Stun Bella",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Pillar-Top Boogie",
+        desc : "Defeat Lil Magma Boogie",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Resentful Pair",
+        desc : "Defeat Devilina",
+        amnt : "1/10/50"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "The Dance of Death",
+        desc : "Get tombstoned by Devilina",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Secret Passages",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    cusp_of_life : {
+    
+      size : 5,
+      name : "Cusp of Life",
+      
+      trophy1 : {
+        
+        name : "Brothers in Arms",
+        desc : "Heal soldiers",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "You Shall Not Pass!",
+        desc : "Restore gates",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Beacon of Hope",
+        desc : "Complete previous trophies",
+        amnt : "2"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "In the Dark",
+        desc : "Defeat Pathos",
+        amnt : "1"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "The Cusp of Life",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    katramus_heights : {
+    
+      size : 3,
+      name : "Katramus Heights",
+      
+      trophy1 : {
+        
+        name : "Greenhouse Rummager",
+        desc : "Search greenhouses",
+        amnt : "1/30/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "To be, Orb Not to Be?",
+        desc : "Destroy Orbs",
+        amnt : "1/30/100"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Putting Evil to Bed",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    ludible_time_hall : {
+    
+      size : 3,
+      name : "Ludible Time Hall",
+      
+      trophy1 : {
+        
+        name : "Time Traveller",
+        desc : "Visit another time",
+        amnt : "1/50/500"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Future Tech",
+        desc : "Fix Andy's machine",
+        amnt : "1/30/100"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "A Friend of Tristan",
+        desc : "Defeat ancient Pekanos",
+        amnt : "1/10/100"
+        
+      }
+
+    },
+    
+    testing_grounds : {
+    
+      size : 2,
+      name : "Testing Grounds",
+      
+      trophy1 : {
+        
+        name : "Exorcist Extraordinaire",
+        desc : "Defeat spirit of rage",
+        amnt : "1/10/50/100/500"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Bravehearted",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      }
+
+    }
+  
+  },
+  
+  three_hundred_runs : {
+    
+    name : "300 Run Dungeons",
+    
+    type : "Dungeon",
+  
+    van_corta : {
+    
+      size : 1,
+      name : "Van Corta",
+      
+      trophy1 : {
+        
+        name : "The Minotaur Menace",
+        desc : "Defeat Hemokan",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    shadow_altar : {
+    
+      size : 2,
+      name : "Shadow Altar",
+      
+      trophy1 : {
+        
+        name : "Turn On the Dark",
+        desc : "Put out a shadow flame",
+        amnt : "100/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Shadows on the Altar",
+        desc : "Defeat Devorak",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    last_of_the_vayar : {
+    
+      size : 1,
+      name : "Last of the Vayar",
+      
+      trophy1 : {
+        
+        name : "A Lone Stone",
+        desc : "Defeat Lost Vayar Guardian",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    warbot_revellion : {
+    
+      size : 1,
+      name : "Warbot Rebellion",
+      
+      trophy1 : {
+        
+        name : "Beta Breaker",
+        desc : "Defeat MK 52 Beta",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    forest_of_chaos : {
+    
+      size : 1,
+      name : "Forest of Chaos",
+      
+      trophy1 : {
+        
+        name : "The Child of Chaos",
+        desc : "Defeat Chaotic Baphomet",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    ignicore : {
+    
+      size : 1,
+      name : "Ignicore",
+      
+      trophy1 : {
+        
+        name : "Dying Fire",
+        desc : "Defeat Pamokan",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    horus_nest : {
+    
+      size : 1,
+      name : "Horus' Nest",
+      
+      trophy1 : {
+        
+        name : "The Fall of the Griffin King",
+        desc : "Defeat Horus",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    ludibrium_clock_tower : {
+    
+      size : 1,
+      name : "Ludibrium Clock Tower",
+      
+      trophy1 : {
+        
+        name : "Fated",
+        desc : "Defeat Papulatus",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    moonlight_foretress : {
+    
+      size : 1,
+      name : "Moonlight Foretress",
+      
+      trophy1 : {
+        
+        name : "Scourge of the Sea",
+        desc : "Defeat Captain Moc",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    illusory_pyramid : {
+    
+      size : 2,
+      name : "Illusory Pyramid",
+      
+      trophy1 : {
+        
+        name : "A Fantasy You",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Summoned By Desire",
+        desc : "Defeat Rhombeetle Lord",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    frozen_temple : {
+    
+      size : 2,
+      name : "Frozen Temple",
+      
+      trophy1 : {
+        
+        name : "Breaking the Ice",
+        desc : "Defeat Titan Goo",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Cold, Lonely Memory",
+        desc : "Defeat Kana",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    blaknov_1 : {
+    
+      size : 1,
+      name : "Blaknov No. 1",
+      
+      trophy1 : {
+        
+        name : "Train Robber",
+        desc : "Defeat Willy One-card",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    blaknov_2 : {
+    
+      size : 1,
+      name : "Blaknov No. 2",
+      
+      trophy1 : {
+        
+        name : "Hidden Card",
+        desc : "Defeat Willy One-card",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    coldwind_cave : {
+    
+      size : 3,
+      name : "Coldwind Cave",
+      
+      trophy1 : {
+        
+        name : "Building Bridges",
+        desc : "Build a bridge",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Who's Decieving Who?",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Came out on Top",
+        desc : "Defeat Vulcantop",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    waterbug_park : {
+    
+      size : 3,
+      name : "Waterbug Park",
+      
+      trophy1 : {
+        
+        name : "Bubblin'",
+        desc : "Trapped in water",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Springer the Desert Bully",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Fast and Furious",
+        desc : "Defeat Irontop",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    casa_de_luna : {
+    
+      size : 2,
+      name : "Casa de Luna",
+      
+      trophy1 : {
+        
+        name : "The Moon is Another World's Secret",
+        desc : "Complete the dungeon",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Moonlight Gatekeeper",
+        desc : "Defeat Sejuke",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    tronix_bunker : {
+    
+      size : 1,
+      name : "Tronix Bunker",
+      
+      trophy1 : {
+        
+        name : "Terminated",
+        desc : "Defeat MK 52 Omega",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    forgotten_vayar : {
+    
+      size : 1,
+      name : "Forgotten Vayar",
+      
+      trophy1 : {
+        
+        name : "Forgotten, But Not Lost",
+        desc : "Defeat Forgotten Vayar",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    blueshade_cave : {
+    
+      size : 1,
+      name : "Blueshade Cave",
+      
+      trophy1 : {
+        
+        name : "Felling an Icy Giant",
+        desc : "Defeat Shoo and Booboo",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    lingering_darkness : {
+    
+      size : 1,
+      name : "Lingering Darkness",
+      
+      trophy1 : {
+        
+        name : "Endless Betrayal",
+        desc : "Defeat Katvan",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    sanctum_of_the_sage : {
+    
+      size : 2,
+      name : "Sanctum of the Sage",
+      
+      trophy1 : {
+        
+        name : "Simply Shocking",
+        desc : "Get tombstoned by Confounding Spirit",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Dream Crusher",
+        desc : "Defeat Animus Wraith",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    }
+  
+  },
+  
+  weird_runs : {
+    
+    name : "Weird Run Dungeons",
+    
+    type : "Dungeon",
+  
+    golden_tower_seven : {
+    
+      size : 1,
+      name : "Golden Tower 7F",
+      
+      trophy1 : {
+        
+        name : "The Long Way Down",
+        desc : "Complete the dungeon",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    golden_tower_four : {
+    
+      size : 1,
+      name : "Golden Tower 4F",
+      
+      trophy1 : {
+        
+        name : "Into the Abyss",
+        desc : "Complete the dungeon",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    katramus_depths : {
+    
+      size : 3,
+      name : "Katramus Depths",
+      
+      trophy1 : {
+        
+        name : "Freedom Regained",
+        desc : "Free slaves",
+        amnt : "1/30/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Undermining the Captain",
+        desc : "Defeat Captain Kabon",
+        amnt : "10/100/500"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Chain Breaker",
+        desc : "Complete the dungeon",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    watchtower_rampart : {
+    
+      size : 4,
+      name : "Watchtower Rampart",
+      
+      trophy1 : {
+        
+        name : "Bombs Away",
+        desc : "Throw Bombs",
+        amnt : "1/100/1000/5000/10000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Missles Away",
+        desc : "Launch Missles",
+        amnt : "1/100/500/1000/5000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Rain of Fire",
+        desc : "Complete previous trophies",
+        amnt : "2"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Defend the Ramparts",
+        desc : "Defeat King Tabo",
+        amnt : "1/10/100/500/1000"
+        
+      }
+
+    },
+    
+    muros_studio : {
+    
+      size : 1,
+      name : "Muros Studio",
+      
+      trophy1 : {
+        
+        name : "Clowns, Why did it..",
+        desc : "Defeat Cloro",
+        amnt : "1/10/50"
+        
+      }
+
+    }
+  
+  },
+  
+  combinations : {
+    
+    name : "Combinations",
+    
+    type : "Dungeon",
+  
+    forest : {
+    
+      size : 1,
+      name : "Dungeons",
+      
+      trophy1 : {
+        
+        name : "Keeper of the Forest",
+        desc : "Complete forest dungeons",
+        amnt : "10"
+        
+      }
+
+    },
+    
+    golden_tower : {
+    
+      size : 1,
+      name : "Golden Towers",
+      
+      trophy1 : {
+        
+        name : "Golden Tower Conqueror",
+        desc : "Complete golden tower dungeons",
+        amnt : "10"
+        
+      }
+
+    }
+  
+  }
+
+}
