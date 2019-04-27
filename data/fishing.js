32804c67bfe1dff8f2d9f08c5b6bcbf55bd794d8
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-27 15:18:00 -0500

    Update fishing.js

--- data/fishing.js
+++ data/fishing.js
@@ -2,26 +2,377 @@ data.fishing = {
   
   name : "Fishing",
   
-  size_c : 15,
-    
-  size_r : 20,
-  
   fishing : {
     
     name : "Fishing",
     
     type : "Type",
   
-    group1 : {
+    fishing : {
     
-      size : 1, //Amount of trophies
-      name : "name", //Group name
+      size : 6,
+      name : "Catch things",
       
       trophy1 : {
         
-        name : "name", //Trophy name
-        desc : "desc", //Trophy description
-        amnt : "amnt" //Trophy amount
+        name : "Catch of the Day",
+        desc : "Catch fish",
+        amnt : "10/100/1000/5000/10000/30000/50000/70000/100000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Prize Angler",
+        desc : "Catch prize fish",
+        amnt : "1/10/100/500/1000"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Sleepfishing",
+        desc : "Auto-catch fish",
+        amnt : "10/100/1000"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "Aimless Angler",
+        desc : "Fail at fishing",
+        amnt : "10/100/1000"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Complete the Fish Album",
+        desc : "Catch different fish",
+        amnt : "5/50/150"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Fishing Maniac's Fantasy",
+        desc : "Catch different prize fish",
+        amnt : "5/50/100"
+        
+      }
+
+    },
+    
+    combinations : {
+    
+      size : 11,
+      name : "Catch certain fish",
+      
+      trophy1 : {
+        
+        name : "Ultimate Extremophiles",
+        desc : "Catch various lava fish",
+        amnt : "9"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "The Most Thrilling Catch",
+        desc : "Catch various foul water fish",
+        amnt : "8"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Shark Master",
+        desc : "Catch various sharks",
+        amnt : "8"
+        
+      },
+      
+      trophy4 : {
+        
+        name : "The Cultured Fishperson",
+        desc : "Catch various carps",
+        amnt : "4"
+        
+      },
+      
+      trophy5 : {
+        
+        name : "Adorable Axolotls",
+        desc : "Catch an Axolotl and Speckled Axolotl",
+        amnt : "2"
+        
+      },
+      
+      trophy6 : {
+        
+        name : "Noble Stalions of the Sea",
+        desc : "Catch Seaseraph and Seademon",
+        amnt : "2"
+        
+      },
+      
+      trophy7 : {
+        
+        name : "Gems of the Sea",
+        desc : "Catch various oysters",
+        amnt : "3"
+        
+      },
+      
+      trophy8 : {
+        
+        name : "Sages of the Sea",
+        desc : "Catch various turtles",
+        amnt : "2"
+        
+      },
+      
+      trophy9 : {
+        
+        name : "A Special Fishing Spot",
+        desc : "Catch special fish",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy10 : {
+        
+        name : "The Golden Reel",
+        desc : "Catch every golden fish",
+        amnt : "12"
+        
+      },
+      
+      trophy11 : {
+        
+        name : "Deep Sea Fashion",
+        desc : "Fish up a seafood-themed weapon",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    locations : {
+    
+      size : 2,
+      name : "Location Fishing",
+      
+      trophy1 : {
+        
+        name : "Trail of Stars",
+        desc : "Catch 3 starfish from Sylvan Woods Trail",
+        amnt : "3"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Passionate Fisher",
+        desc : "Catch 50 fish in Windsong Ravine",
+        amnt : "50"
+        
+      }
+
+    }
+    
+  },
+  
+  o_prize_fish : {
+    
+    name : "Overworld Prize Fish",
+    
+    type : "Location",
+    
+    octopirate : {
+    
+      size : 1,
+      name : "Lith Harbor",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Octopirate!",
+        desc : "Catch a prize Octopirate",
+        amnt : "1"
+        
+      }
+      
+    },
+    
+    knight_fish : {
+    
+      size : 1,
+      name : "Tria",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Knight Fish!",
+        desc : "Catch a prize Knight Fish",
+        amnt : "1"
+        
+      }
+
+    },
+      
+    jailfish : {
+    
+      size : 1,
+      name : "Alikar",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Jailfish!",
+        desc : "Catch a prize Jailfish",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    broccoturtle : {
+    
+      size : 1,
+      name : "Turtcoli Cave",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Broccoturtle!",
+        desc : "Catch a prize Broccoturtle",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    mecha_shark : {
+    
+      size : 1,
+      name : "Nerman Forge",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Mecha Shark!",
+        desc : "Catch a prize Mecha Shark",
+        amnt : "1"
+        
+      }
+
+    }
+    
+  
+  },
+  
+  d_prize_fish : {
+    
+    name : "Dungeon Prize Fish",
+    
+    type : "Dungeon",
+  
+    machodile : {
+    
+      size : 1,
+      name : "Van Corta",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Machodile!",
+        desc : "Catch a prize Machodile",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    seahorus : {
+    
+      size : 1,
+      name : "Horus's Nest",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Seahorus!",
+        desc : "Catch a prize Seahorus",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    treelacanth : {
+    
+      size : 1,
+      name : "Ludible Time Hall (Past Jungle/Henesys)",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Treelacanth!",
+        desc : "Catch a prize Treelacanth",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    masked_parexus : {
+    
+      size : 1,
+      name : "Blueshade Cave",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Masked Parexus!",
+        desc : "Catch a prize Masked Parexus",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    pyromander : {
+    
+      size : 1,
+      name : "Fire Dragon",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Pyromander!",
+        desc : "Catch a prize Pyromander",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    vilequill : {
+    
+      size : 1,
+      name : "Temple of Immortals",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Vilequill!",
+        desc : "Catch a prize Vilequill",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    footballfish : {
+    
+      size : 1,
+      name : "Hard Chaos Captain Moc",
+      
+      trophy1 : {
+        
+        name : "Hook, Line, and Footballfish!",
+        desc : "Catch a prize Footballfish",
+        amnt : "1"
         
       }
 
@@ -29,25 +380,134 @@ data.fishing = {
   
   },
   
-  prize_fish : {
+  golden_fish : {
     
-    name : "Prize Fish",
+    name : "Golden Fish",
     
-    type : "Fish",
-  
-    group1 : {
+    type : "Mastery Bait",
     
-      size : 1, //Amount of trophies
-      name : "name", //Group name
+    beginner : {
+    
+      size : 3,
+      name : "Beginner",
       
       trophy1 : {
         
-        name : "name", //Trophy name
-        desc : "desc", //Trophy description
-        amnt : "amnt" //Trophy amount
+        name : "Golden Ricefish Fisher",
+        desc : "Catch Golden Ricefish",
+        amnt : "1/3/10"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Golden Seaweed Fisher",
+        desc : "Catch Golden Seaweed",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Golden Octopus Fisher",
+        desc : "Catch Golden Octopus",
+        amnt : "1"
         
       }
-
+      
+    },
+    
+    veteran : {
+    
+      size : 3,
+      name : "Veteran",
+      
+      trophy1 : {
+        
+        name : "Golden Bream Fisher",
+        desc : "Catch Golden Bream",
+        amnt : "1/3/10"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Golden Conch Fisher",
+        desc : "Catch Golden Conch",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Golden Starfish Fisher",
+        desc : "Catch Golden Starfish",
+        amnt : "1"
+        
+      }
+      
+    },
+    
+    expert : {
+    
+      size : 3,
+      name : "Expert",
+      
+      trophy1 : {
+        
+        name : "Golden Lobster Fisher",
+        desc : "Catch Golden Lobster",
+        amnt : "1/3/10"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Golden Abalone Fisher",
+        desc : "Catch Golden Abalone",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Golden Turtle Fisher",
+        desc : "Catch Golden Turtle",
+        amnt : "1"
+        
+      }
+      
+    },
+    
+    master : {
+    
+      size : 3,
+      name : "Master",
+      
+      trophy1 : {
+        
+        name : "Golden Seahorse Fisher",
+        desc : "Catch Golden Seahorse",
+        amnt : "1/3/10"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Golden Clam Fisher",
+        desc : "Catch Golden Clam",
+        amnt : "1"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Golden Dolphin Fisher",
+        desc : "Catch Golden Dolphin",
+        amnt : "1"
+        
+      }
+      
     }
   
   }
