64be6898bef6e326ad9eb8bc69c6b95e7be4187d
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-27 16:03:17 -0500

    Update guild_raids.js

--- data/guild_raids.js
+++ data/guild_raids.js
@@ -1,1 +1,28 @@
-data.guild_raids = {}
+data.guild_raids = {
+  
+  name : "name", //Sheet name
+  
+  category : { //A category stretches the entire row
+    
+    name : "name", //Category name
+    
+    type : "type", //Category name description
+  
+    group1 : {
+    
+      size : 1, //Amount of trophies
+      name : "name", //Group name
+      
+      trophy1 : {
+        
+        name : "name", //Trophy name
+        desc : "desc", //Trophy description
+        amnt : "amnt" //Trophy amount
+        
+      }
+
+    }
+  
+  }
+  
+}
