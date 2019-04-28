adfbbac9490f8d0c9a8dd35a38ef00ab93bcc188
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-28 18:03:29 -0500

    Update guild_raids.js

--- data/guild_raids.js
+++ data/guild_raids.js
@@ -6,18 +6,188 @@ data.guild_raids = {
     
     name : "L1: Apuella Outback",
     
-    type : "type", //Category name description
+    type : "Runs required",
   
-    group1 : {
+    grindy : {
     
-      size : 1, //Amount of trophies
-      name : "name", //Group name
+      size : 2,
+      name : "Multiple",
       
       trophy1 : {
         
-        name : "name", //Trophy name
-        desc : "desc", //Trophy description
-        amnt : "amnt" //Trophy amount
+        name : "Master of Energy Control",
+        desc : "Fill a jar with energy",
+        amnt : "1/10/50/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Brave Dragon",
+        desc : "Fill a jar with dragon energy",
+        amnt : "1/10/50/100"
+        
+      }
+
+    },
+    
+    single : {
+    
+      size : 2,
+      name : "One run",
+      
+      trophy1 : {
+        
+        name : "You Can't Help But Dance!",
+        desc : "Make the cacti dance",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Acknowledged One",
+        desc : "Complete L1",
+        amnt : "1"
+        
+      }
+
+    }
+  
+  },
+  
+  l_two : {
+    
+    name : "L2: Rainbow Slime Factory",
+    
+    type : "Runs required",
+  
+    grindy : {
+    
+      size : 3,
+      name : "Multiple",
+      
+      trophy1 : {
+        
+        name : "I Can't Let You Go!",
+        desc : "Defeat Mutant Slimes",
+        amnt : "1/50/100/500/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "I Am the Manager",
+        desc : "Output a result",
+        amnt : "1/5/10"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Rainbow Alchemist",
+        desc : "Complete L2",
+        amnt : "1/10/50/100"
+        
+      }
+
+    },
+    
+    success : {
+    
+      size : 1,
+      name : "One Success",
+      
+      trophy1 : {
+        
+        name : "Why is Rainbow Slime Conversion Forbidden?",
+        desc : "Become Rainbow Slime",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Forbidden Alchemy",
+        desc : "Complete L2",
+        amnt : "1"
+        
+      }
+
+    },
+    
+    fail : {
+    
+      size : 2,
+      name : "One Fail",
+      
+      trophy1 : {
+        
+        name : "So Clingy...",
+        desc : "Become Mutant Slime",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Too Much is as Bad as Too Little",
+        desc : "Fail at L2",
+        amnt : "1"
+        
+      }
+
+    }
+  
+  },
+  
+  l_three : {
+    
+    name : "L3: Battle Arena",
+    
+    type : "Runs required",
+  
+    grindy : {
+    
+      size : 1,
+      name : "Multiple",
+      
+      trophy1 : {
+        
+        name : "One More Knockdown!",
+        desc : "Defeat enemies in L3",
+        amnt : "1/10/50/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Are You the Last?",
+        desc : "Defeat Amadon in L3",
+        amnt : "1/10/50/100"
+        
+      }
+
+    },
+    
+    single : {
+    
+      size : 2,
+      name : "One run",
+      
+      trophy1 : {
+        
+        name : "I Am the Champion!!",
+        desc : "Complete L3",
+        amnt : "1"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "One of Ten in a 10-to-1 Fight",
+        desc : "Succeed with 10 party members",
+        amnt : "1"
         
       }
 
