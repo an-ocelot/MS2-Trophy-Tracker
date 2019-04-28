9e2c25d758ba0f29e4b53b3a84210bb8eb7bf5d0
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-04-28 18:38:42 -0500

    Update hard_dungeons.js

--- data/hard_dungeons.js
+++ data/hard_dungeons.js
@@ -1,1 +1,159 @@
-data.hard_dungeons = {}
+data.hard_dungeons = {
+  
+  name : "Hard Dungeons",
+  
+  fd_lab : {
+    
+    name : "1800 GS",
+    
+    type : "Dungeon",
+  
+    fire_dragon : {
+    
+      size : 3,
+      name : "The Fire Dragon",
+      
+      trophy1 : {
+        
+        name : "A Blazing Inferno",
+        desc : "Battle Pyrros Fard",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "A Sputtering Flame",
+        desc : "Defeat Pyrros Fard",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Firefighter",
+        desc : "Last hit Pyrros Fard",
+        amnt : "1/2/5"
+        
+      }
+
+    },
+    
+    labyrinthine_halls : {
+    
+      size : 2,
+      name : "Labyrinthine Halls",
+      
+      trophy1 : {
+        
+        name : "Into the Maze",
+        desc : "Defeat Kabo",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "The One Who Escaped the Maze",
+        desc : "Last hit Kabo",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    }
+  
+  },
+  
+  rog_lube : {
+    
+    name : "2100 GS",
+    
+    type : "Dungeon",
+  
+    temple_of_immortals : {
+    
+      size : 3,
+      name : "Temple of Immortals",
+      
+      trophy1 : {
+        
+        name : "A Deadly Hook",
+        desc : "Get tombstoned by swinging hook",
+        amnt : "1/10/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Tristan's True Pal",
+        desc : "Defeat Balrog",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy3 : {
+        
+        name : "Balrog Breaker",
+        desc : "Last hit Balrog",
+        amnt : "1/2/5"
+        
+      }
+
+    },
+    
+    lubelisk : {
+    
+      size : 1,
+      name : "Lubelisk",
+      
+      trophy1 : {
+        
+        name : "Darkness Swallowing the Light",
+        desc : "Defeat Varrekant",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    }
+  
+  },
+  
+  tris_rune : {
+    
+    name : "2800 GS",
+    
+    type : "Dungeon",
+  
+    beyondlink_tris : {
+    
+      size : 1,
+      name : "BeyondLink Tris",
+      
+      trophy1 : {
+        
+        name : "The Fall of Kandura",
+        desc : "Defeat Kandura",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    rune_temple : {
+    
+      size : 1,
+      name : "Rune Temple",
+      
+      trophy1 : {
+        
+        name : "Clash of the Titan",
+        desc : "Defeat Nutaman",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    }
+  
+  }
+
+}
