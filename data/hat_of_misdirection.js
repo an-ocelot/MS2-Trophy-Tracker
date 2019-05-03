1aea7a7b5afa535694b10ded33ed08642b717e7a
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-05-02 21:16:13 -0500

    Update hat_of_misdirection.js

--- data/hat_of_misdirection.js
+++ data/hat_of_misdirection.js
@@ -2,22 +2,120 @@ data.hat_of_misdirection = {
   
   name : "Hat of Misdirection",
   
-  category : { //A category stretches the entire row
+  visit : {
     
-    name : "name", //Category name
+    name : "Hat of Misdirection",
     
-    type : "type", //Category name description
+    type : "Realm",
   
-    group1 : {
+    pocket_realm : {
     
-      size : 1, //Amount of trophies
-      name : "name", //Group name
+      size : 1,
+      name : "Any",
       
       trophy1 : {
         
-        name : "name", //Trophy name
-        desc : "desc", //Trophy description
-        amnt : "amnt" //Trophy amount
+        name : "A Wrinkle in Space",
+        desc : "Visit any Pocket Realm",
+        amnt : "1/10/100/300/1000"
+        
+      }
+
+    },
+    
+    fractured_ludibrium : {
+    
+      size : 1,
+      name : "Fractured Ludibrium",
+      
+      trophy1 : {
+        
+        name : "Broken Blocks",
+        desc : "Visit Fractured Ludibrium",
+        amnt : "1/30/100"
+        
+      }
+
+    },
+    
+    money_tree_mayhem : {
+    
+      size : 1,
+      name : "Money Tree Mayhem",
+      
+      trophy1 : {
+        
+        name : "I Guess it Does Grow on Trees",
+        desc : "Visit Money Tree Mayhem",
+        amnt : "1/30/100"
+        
+      }
+
+    },
+    
+    kumbawanka : {
+    
+      size : 2,
+      name : "Kumbawanka's Vault",
+      
+      trophy1 : {
+        
+        name : "Kumbakawanka Coveter",
+        desc : "Open chests in Kumbawanka's Vault",
+        amnt : "100/1000"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "The Ancient Treasure",
+        desc : "Visit Kumbawanka's Vault",
+        amnt : "1/30/100"
+        
+      }
+
+    },
+    
+    surnuny_dream : {
+    
+      size : 1,
+      name : "Surnuny's Dream",
+      
+      trophy1 : {
+        
+        name : "Surnuny's Dreamgirl",
+        desc : "Visit Surnuny's Dream",
+        amnt : "1/30/100"
+        
+      }
+
+    },
+    
+    rainbow_slime_factory : {
+    
+      size : 1,
+      name : "Rainbow Slime Factory",
+      
+      trophy1 : {
+        
+        name : "Rainbow Slime Factory",
+        desc : "Visit Rainbow Slime Factory",
+        amnt : "1/30/100"
+        
+      }
+
+    },
+    
+    suborbital_space_drop : {
+    
+      size : 1,
+      name : "Suborbital Space Drop",
+      
+      trophy1 : {
+        
+        name : "Spacediver",
+        desc : "Visit Suborbital Space Drop",
+        amnt : "1/30/100"
         
       }
 
