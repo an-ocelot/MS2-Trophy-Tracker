34f307f9bf291f36631d594116610bc5ad0aca95
An Ocelot <42921234+an-ocelot@users.noreply.github.com>
2019-05-02 20:41:30 -0500

    Update chaos_dungeons.js

--- data/chaos_dungeons.js
+++ data/chaos_dungeons.js
@@ -2,27 +2,102 @@ data.chaos_dungeons = {
   
   name : "Chaos Dungeons",
   
-  category : { //A category stretches the entire row
+  hard_dungeons : {
     
-    name : "name", //Category name
+    name : "Hard Chaos Raids",
     
-    type : "type", //Category name description
+    type : "Dungeon",
   
-    group1 : {
+    shadow_altar : {
     
-      size : 1, //Amount of trophies
-      name : "name", //Group name
+      size : 2,
+      name : "Hard Shadow Altar",
       
       trophy1 : {
         
-        name : "name", //Trophy name
-        desc : "desc", //Trophy description
-        amnt : "amnt" //Trophy amount
+        name : "The Invader's Demise",
+        desc : "Recieve rewards",
+        amnt : "10/25/50/75/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Shadows in the Darkness",
+        desc : "Defeat Devorak",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    moonlight_fortress : {
+    
+      size : 2,
+      name : "Hard Moonlight Fortress",
+      
+      trophy1 : {
+        
+        name : "The Captain's Demise",
+        desc : "Recieve rewards",
+        amnt : "10/25/50/75/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Scourge of the Sea",
+        desc : "Defeat Captain Moc",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    clock_tower : {
+    
+      size : 2,
+      name : "Ludibrium Clock Tower",
+      
+      trophy1 : {
+        
+        name : "The Mischief Maker's Demise",
+        desc : "Recieve rewards",
+        amnt : "10/25/50/75/100"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Clock Tower of Chaos",
+        desc : "Defeat Papulatus",
+        amnt : "1/10/50/100/300"
+        
+      }
+
+    },
+    
+    infernog : {
+    
+      size : 2,
+      name : "Wrath of Infernog",
+      
+      trophy1 : {
+        
+        name : "Crimson Balrog Hunter",
+        desc : "Defeat Crimson Balrogs",
+        amnt : "1/10/50/100/300"
+        
+      },
+      
+      trophy2 : {
+        
+        name : "Vanquisher of Infernog",
+        desc : "Complete Wrath of Infernog",
+        amnt : "10/50/100/200/300"
         
       }
 
     }
-  
-  }
 
 }
